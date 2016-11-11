a389b2b0e18416bac277ed1e18ff1f8400a2b765
Leon Guo <chineseroyalstar@gmail.com>
2016-11-11 14:00:33 +0800

    Initial Commit

---
+++ GZUIViewFrameUtility/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GZUIViewFrameUtility
+//
+//  Created by armada on 2016/11/11.
+//  Copyright © 2016年 com.zlot.gz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
