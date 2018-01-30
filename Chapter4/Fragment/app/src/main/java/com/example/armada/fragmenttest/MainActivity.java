c92317c7ea009451967d1653581556b49993c934
Leon Guo <chineseroyalstar@gmail.com>
2018-01-30 23:39:07 +0800

    Chapter4

---
+++ Chapter4/Fragment/app/src/main/java/com/example/armada/fragmenttest/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.armada.fragmenttest;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button =(Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                replaceFragment(new AnotherRightFragment());
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        //它可以接受一个名字用于返回栈的状态, 一般传入null即可,
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
