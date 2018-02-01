bbafe842ff88cf1fe722d4d6cbfcd68abc69aff4
Leon Guo <chineseroyalstar@gmail.com>
2018-02-01 23:57:17 +0800

    Chapter5

--- Chapter5/BroadcastTest/app/src/main/java/com/example/armada/broadcasttest/MainActivity.java
+++ Chapter5/BroadcastTest/app/src/main/java/com/example/armada/broadcasttest/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,9 +15,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private IntentFilter intetnFilter;
+    private IntentFilter intentFilter;
 
     private NetworkChangeReceiver networkChangeReceiver;
+
+    private LocalReceiver localReceiver;
+
+    private LocalBroadcastManager localBroadcastManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,27 +29,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /*
-        intetnFilter = new IntentFilter();
-        intetnFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver,intetnFilter);*/
+            intetnFilter = new IntentFilter();
+            intetnFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+            networkChangeReceiver = new NetworkChangeReceiver();
+            registerReceiver(networkChangeReceiver,intetnFilter);
+        */
+
+        localBroadcastManager = LocalBroadcastManager.getInstance(this); //获取实例
 
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
-               // sendBroadcast(intent);
-                sendBroadcast(intent,null);
+
+               // sendBroadcast(intent); //发送广播
+
+               // Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
+               // sendBroadcast(intent,null); //发送有序广播
+
+               Intent intent = new Intent("com.example.broadcasttest.LOCAL_BROADCAST");
+               localBroadcastManager.sendBroadcast(intent);
             }
         });
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcasttest.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver,intentFilter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
+        localBroadcastManager.unregisterReceiver(localReceiver);
     }
 
     //定义内部类,继承自BroadcastReceiver, 并重写了父类的onReceive()方法. 这样每当网络状态发生改变时, onReceive()方法就会得到执行
@@ -60,6 +78,14 @@ public class MainActivity extends AppCompatActivity {
             }else {
                 Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    class LocalReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "received local broadcast", Toast.LENGTH_SHORT).show();
         }
     }
 }
