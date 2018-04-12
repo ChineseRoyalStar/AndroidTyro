e8c90a3554bbe778c4ef16067a3f1a138d93633a
Leon Guo <chineseroyalstar@gmail.com>
2018-04-13 00:14:57 +0800

    Chapter10

---
+++ Chapter10/ServiceTest/app/src/main/java/com/example/aiait/servicetest/MyIntentService.java
@@ -0,0 +1,25 @@
+package com.example.aiait.servicetest;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class MyIntentService extends IntentService {
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        // 打印当前线程的id
+        Log.d("MyIntentService", "Thread id is " + Thread.currentThread().getId());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyIntentService", "onDestroy excuted");
+    }
+}
--- Chapter10/ServiceTest/app/src/main/java/com/example/aiait/servicetest/MyService.java
+++ Chapter10/ServiceTest/app/src/main/java/com/example/aiait/servicetest/MyService.java
@@ -1,29 +1,72 @@
 package com.example.aiait.servicetest;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class MyService extends Service {
+
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+
+        public void startDownload() {
+            Log.d("MyService", "startDownload excuted");
+        }
+
+        public int getProgress() {
+            Log.d("MyService", "getProgress excuted");
+            return 0;
+        }
+    }
+
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+       // throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d("MyService", "onCreate executed");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .build();
+        startForeground(1, notification);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("MyService", "onStartCommand executed");
+
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                // 处理具体的逻辑
+//                stopSelf(); //处理完逻辑停下来
+//            }
+//        }).start();
+
         return super.onStartCommand(intent, flags, startId);
     }
 
